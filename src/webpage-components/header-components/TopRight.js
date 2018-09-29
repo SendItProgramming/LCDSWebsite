7bac93062071320b625f323b614e360ae225d2ba
Jack Staples <jackstaplesjcrs@gmail.com>
2018-09-29 15:48:42 -0700

    create base of navigation component using react-bootstrap

--- src/webpage-components/header-components/TopRight.js
+++ src/webpage-components/header-components/TopRight.js
@@ -1,11 +1,44 @@
 import React, { Component } from "react"
+import {Nav, NavItem, NavDropdown, MenuItem} from "react-bootstrap"
 
-export default class TopRight extends Component {
+export default class TopRight extends Component {	
     render() {
         return (
-            <div>
-                <p>This is the top right section of the header</p>
+            <div name="topRight">
+				<Nav bsStyle="pills" onSelect={this.handleSelect}>
+					<NavItem eventKey="Home">Home</NavItem>
+					<NavDropdown eventKey="About Us" title="About Us" id="nav-dropdown">
+						<MenuItem eventKey="Our Staff">Our Staff</MenuItem>
+						<MenuItem eventKey="Our Board">Our Board</MenuItem>
+						<MenuItem eventKey="Sammy Squirrel">Sammy Squirrel</MenuItem>
+						<MenuItem eventKey="Testimonials">Testimonials</MenuItem>
+						<MenuItem eventKey="Our History">Our History</MenuItem>
+					</NavDropdown>
+					<NavDropdown eventKey="Programs" title="Programs" id="nav-dropdown">
+						<MenuItem eventKey="Kindergarten">Kindergarten</MenuItem>
+						<MenuItem eventKey="Junior Kindergarten (4-5)">Junior Kindergarten (4-5)</MenuItem>
+						<MenuItem eventKey="Pre-school (3-4)">Pre-school (3-4)</MenuItem>
+						<MenuItem eventKey="Learning Support">Learning Support</MenuItem>
+						<MenuItem eventKey="Field Trips">Field Trips</MenuItem>
+						<MenuItem eventKey="Daily Snacks">Daily Snacks</MenuItem>
+					</NavDropdown>
+					<NavDropdown eventKey="Registration" title="Registration" id="nav-dropdown">
+						<MenuItem eventKey="Program Fees">Program Fees</MenuItem>
+						<MenuItem eventKey="Parent Involvement">Parent Involvement</MenuItem>
+					</NavDropdown>
+					<NavItem eventKey="Resources">Resources</NavItem>
+					<NavDropdown eventKey="Events" title="Events" id="nav-dropdown">
+						<MenuItem eventKey="Job Opportunities">Job Opportunities</MenuItem>
+						<MenuItem eventKey="Fundraising">Fundraising</MenuItem>
+					</NavDropdown>
+					<NavItem eventKey="News">News</NavItem>
+					<NavItem eventKey="Contact">Contact</NavItem>
+				</Nav>
             </div>
         )
     }
+	
+	handleSelect(selectedKey) {
+		console.log(`selected ${selectedKey}`);
+	}
 }
