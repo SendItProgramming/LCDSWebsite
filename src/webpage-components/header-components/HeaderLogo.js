13083e4c512b972650bd6a4b8e54e79e7f347d20
Jack Staples <jackstaplesjcrs@gmail.com>
2018-09-29 16:02:58 -0700

    create logo component using react-bootstrap Image, currently using place holder image

--- src/webpage-components/header-components/HeaderLogo.js
+++ src/webpage-components/header-components/HeaderLogo.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react"
+import {Image} from "react-bootstrap"
 
 export default class HeaderLogo extends Component {
     render() {
         return (
             <div>
-                <p>This is the header logo</p>
+				<Image src="http://placekitten.com/200/200" alt="Home Page" thumbnail />
             </div>
         )
     }
