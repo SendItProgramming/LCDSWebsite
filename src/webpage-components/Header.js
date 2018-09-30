c469e939780f7d86ef0ae11d9f85b29d8bb95040
Jack Staples <jackstaplesjcrs@gmail.com>
2018-09-29 18:05:04 -0700

    cleanup commit, add div names and remove unnecessary text in header component

--- src/webpage-components/Header.js
+++ src/webpage-components/Header.js
@@ -6,8 +6,7 @@ import TopRight from "./header-components/TopRight";
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                <h1>This is the header</h1>
+            <div id="header">
                 <HeaderLogo/>
                 <TopRight/>
             </div>
--- src/webpage-components/header-components/HeaderLogo.js
+++ src/webpage-components/header-components/HeaderLogo.js
@@ -4,7 +4,7 @@ import {Image} from "react-bootstrap"
 export default class HeaderLogo extends Component {
     render() {
         return (
-            <div>
+            <div id="logo">
 				<Image src="http://placekitten.com/200/200" alt="Home Page" thumbnail />
             </div>
         )
