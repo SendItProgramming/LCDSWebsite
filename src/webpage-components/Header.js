84e42c13cf6a1e69c9f3ef5e907288cd35b0c848
MarkTigchelaar <tigchelaarm@mymacewan.ca>
2018-10-09 21:14:58 -0600

    Replace stateless header child class with header method

--- src/webpage-components/Header.js
+++ src/webpage-components/Header.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react"
-import HeaderLogo from "./header-components/HeaderLogo";
 import TopRight from "./header-components/TopRight";
-
+import {Image} from "react-bootstrap"
 
 export default class Header extends Component {
     render() {
         return (
             <div id="header">
-                <HeaderLogo/>
+                {this.HeaderLogo()}
                 <TopRight site_urls={this.props.site_urls}/>
+            </div>
+        )
+    }
+
+    HeaderLogo() {
+        return (
+            <div id="logo">
+				<Image src="http://placekitten.com/200/200" alt="Home Page" thumbnail />
             </div>
         )
     }
