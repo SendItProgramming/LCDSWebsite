410310bed482bbc2c608440851eb4acd31532170
MarkTigchelaar <tigchelaarm@mymacewan.ca>
2018-10-14 21:23:37 -0600

    Move urls to dummy db connection file

--- src/WebPage.js
+++ src/WebPage.js
@@ -4,6 +4,7 @@ import Header from "./webpage-components/Header";
 import Content from "./webpage-components/Content";
 import Footer from "./webpage-components/Footer";
 import CustomBottom from "./webpage-components/CustomBottom";
+import { routes } from "./database-connection/Dummy";
 import './WebPage.css';
 
 export default class WebPage extends Component {
@@ -26,41 +27,4 @@ export default class WebPage extends Component {
       </BrowserRouter>
     );
   }
-}
-
-
-function routes() {
-  return {
-    Home:               "/",
-
-    AboutUs:            "/about-us/",
-    OurStaff:           "/about-us/our-staff",
-    OurBoard:           "/about-us/our-board",
-    Squirrel:           "/about-us/sammy-squirrel",
-    Testimonials:       "/about-us/testimonials",
-    History:            "/about-us/our-history",
-    Founder:            "/about-us/founder",
-
-    Programs:           "/programs/",
-    Kindergarden:       "/programs/kindergarden",
-    JrKindergarden:     "/programs/jr-kindergarden",
-    PreSchool:          "/programs/preschool",
-    LearningSupport:    "/programs/learning-support",
-    FieldTrips:         "/programs/field-trips",
-    DailySnacks:        "/programs/daily-snacks",
-
-    Registration:       "/registration/",
-    ProgramFees:        "/registration/program-fees",
-    ParentInvolvement:  "/registration/parent-involvement",
-
-    Resources:          "/resources",
-
-    Events:             "/events/",
-    JobOpportunities:   "/events/job-opportunities",
-    Fundraising:        "/events/fundraising",
-    Calendar:           "/events/calendar",
-
-    News:               "/news",
-    Contact:            "/contact"
-  };
 }
