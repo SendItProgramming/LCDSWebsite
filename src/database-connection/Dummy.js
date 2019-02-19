51fa200088ad40f141e51d097470d4c21c97729d
Jordanhewko <hewkoj2@mymacewan.ca>
2019-02-18 20:49:16 -0700

    Move Resources page into router, add basic 404 page

--- src/database-connection/Dummy.js
+++ src/database-connection/Dummy.js
@@ -34,7 +34,9 @@ export function routes() {
     Calendar:           "/events/calendar",
 
     News:               "/news",
-    Contact:            "/contact"
+    Contact:            "/contact",
+
+    NotFound:           "/*/"
   };
 }
 
