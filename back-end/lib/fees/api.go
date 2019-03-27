b7abb88d1f09b7d88f973714dcaaf59825d7dd02
Jordanhewko <hewkoj2@mymacewan.ca>
2019-03-26 18:00:28 -0600

    Add fees api

---
+++ back-end/lib/fees/api.go
@@ -0,0 +1,37 @@
+package fees
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type API struct {
+	fd  FeesDB
+	mux *mux.Router
+}
+
+func (a API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	a.mux.ServeHTTP(w, r)
+}
+
+func (fd FeesDB) GetFees(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	n, err := fd.GetAllFees()
+	if err != nil {
+		fmt.Println("Problem in grabbing the Fees: ", err)
+		return
+	}
+	json.NewEncoder(w).Encode(n)
+}
+
+func NewAPI(db *sql.DB) API {
+	m := mux.NewRouter()
+	f := FeesDB{db}
+	m.Handle("/", http.HandlerFunc(f.GetFees))
+
+	return API{f, m}
+}
---
+++ back-end/lib/fees/fees.go
@@ -0,0 +1,48 @@
+package fees
+
+import (
+	"database/sql"
+	"errors"
+)
+
+var FeesNotFoundError = errors.New("Could not find the Fees")
+
+type Fees struct {
+	ID         int
+	FeesClass  string
+	FeesCost   string
+	FeesPeriod string
+}
+type FeesDB struct {
+	db *sql.DB
+}
+
+// func (f FeesDb) GetFeesByYear(SchoolYear string) ([]FeesPost, error){
+// 	q := "SELECT fee_id, feesClass, feesCost, feesPeriod FROM fees WHERE feesClass= $1"
+// 	var fees []FeesPost
+
+// }
+func (f FeesDB) GetAllFees([]FeesPost, error) {
+	q := `Select ID, FeesClass, FeesCost, FeesPeriod FROM fees`
+
+	var fees []FeesPost
+
+	rows, err := f.db.Query(q)
+	if err == sql.ErrNoRows {
+		return fees, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var fp FeesPost
+		err = rows.Scan(&fp.ID, &fp.FeesClass, &fp.FeesCost, &fp.FeesPeriod)
+		if err != nil {
+			return fees, err
+		}
+		fees = append(fees, fp)
+	}
+	err = rows.Err()
+	if err != nil {
+		return fees, err
+	}
+	return fees, nil
+}
