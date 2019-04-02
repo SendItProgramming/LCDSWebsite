a07fed366bf0eb567448d08a33e30f143b837198
Brett-A-T-Anderson <baanderson237@gmail.com>
2019-04-02 14:47:07 -0600

    Add route for posting events

--- back-end/lib/events/api.go
+++ back-end/lib/events/api.go
@@ -31,13 +31,34 @@ func NewAPI(db *sql.DB) API {
 }
 
 func (ed EventDB) ServeEvents(w http.ResponseWriter, r *http.Request) {
-	startDate := time.Now().AddDate(0, -6, 0)
-	endDate := time.Now().AddDate(0, 6, 0)
-	e, err := ed.GetEvents(startDate, endDate)
-	if err != nil {
-		fmt.Println("We encountered an error grabbing events: ", err)
+	if r.Method == "GET" {
+		startDate := time.Now().AddDate(0, -6, 0)
+		endDate := time.Now().AddDate(0, 6, 0)
+		e, err := ed.GetEvents(startDate, endDate)
+		if err != nil {
+			fmt.Println("We encountered an error grabbing events: ", err)
+			return
+		}
+		json.NewEncoder(w).Encode(e)
+	}
+	if r.Method == "POST" {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		dec := json.NewDecoder(r.Body)
+		defer r.Body.Close()
+		var e Event
+		if err := dec.Decode(&e); err != nil {
+			fmt.Println("There was an error decoding Edit Post: ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err := ed.SubmitEvent(e)
+		if err != nil {
+			fmt.Println("Error submitting the request: ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
 		return
 	}
-	json.NewEncoder(w).Encode(e)
 
 }
