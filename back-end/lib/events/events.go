d5bd40a06b9068ff3a28b85dbf53e0832f122a0e
Brett-A-T-Anderson <baanderson237@gmail.com>
2019-04-07 10:00:56 -0600

    Change event json encoding

--- back-end/lib/events/events.go
+++ back-end/lib/events/events.go
@@ -6,13 +6,17 @@ import (
 )
 
 type Event struct {
-	EventID     int
-	Title       string
-	StartDate   time.Time
-	EndDate     time.Time
-	URL         string
-	Location    string
-	Description string
+	EventID     int       `json:"eventID"`
+	Title       string    `json:"title"`
+	Start       time.Time `json:"start"`
+	End         time.Time `json:"end"`
+	URL         string    `json:"url"`
+	Location    string    `json:"location"`
+	Description string    `json:"description"`
+}
+
+type EventArray struct {
+	EventArr []Event `json:"event"`
 }
 
 type EventDB struct {
@@ -23,22 +27,23 @@ func NewEventDB(db *sql.DB) EventDB {
 	return EventDB{db}
 }
 
-func (eDB EventDB) GetEvents(startDate, endDate time.Time) ([]Event, error) {
+func (eDB EventDB) GetEvents(startDate, endDate time.Time) (EventArray, error) {
 	var e []Event
+	var eArr EventArray
 	q := `SELECT event_id, title, start_date, end_date, url, event_location, description
 			FROM events
 			where start_date >= $1 AND end_date <= $2`
 	rows, err := eDB.db.Query(q, startDate, endDate)
 	if err != nil {
-		return e, err
+		return eArr, err
 	}
 	defer rows.Close()
 	for rows.Next() {
 		var singleEvent Event
 		var nullUrl, nullLocation sql.NullString
-		err := rows.Scan(&singleEvent.EventID, &singleEvent.Title, &singleEvent.StartDate, &singleEvent.EndDate, &nullUrl, &nullLocation, &singleEvent.Description)
+		err := rows.Scan(&singleEvent.EventID, &singleEvent.Title, &singleEvent.Start, &singleEvent.End, &nullUrl, &nullLocation, &singleEvent.Description)
 		if err != nil {
-			return e, err
+			return eArr, err
 		}
 		if nullUrl.Valid {
 			singleEvent.URL = nullUrl.String
@@ -49,15 +54,16 @@ func (eDB EventDB) GetEvents(startDate, endDate time.Time) ([]Event, error) {
 		e = append(e, singleEvent)
 	}
 	if rows.Err() != nil {
-		return e, rows.Err()
+		return eArr, rows.Err()
 	}
-	return e, nil
+	eArr.EventArr = e
+	return eArr, nil
 }
 
 func (eDB EventDB) SubmitEvent(e Event) error {
 	q := `INSERT INTO events(title, start_date, end_date, url, event_location, description)
 			VALUES ($1, $2, $3, $4, $5, $6)`
-	_, err := eDB.db.Query(q, e.Title, e.StartDate, e.EndDate, e.URL, e.Location, e.Description)
+	_, err := eDB.db.Query(q, e.Title, e.Start, e.End, e.URL, e.Location, e.Description)
 	if err != nil {
 		return err
 	}
