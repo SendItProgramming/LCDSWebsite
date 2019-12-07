c3339fcb7048fff24cee78c0347df0fee858977f
Jordanhewko <hewkoj2@mymacewan.ca>
2019-12-06 21:18:09 -0700

    add to migration

--- back-end/migrations/00_DB_Creation.sql
+++ back-end/migrations/00_DB_Creation.sql
@@ -1,3 +1,4 @@
+create extension pgcrypto;
 CREATE TABLE users(
 	user_id SERIAL CONSTRAINT increment_id PRIMARY KEY,
     email VARCHAR  NOT NULL, -- Constraints : Unique or null, not null if login feature required. Disregard if ! active
