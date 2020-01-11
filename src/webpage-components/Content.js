5a988988a82f96f60201934204896bfc35e80173
Jordanhewko <hewkoj2@mymacewan.ca>
2020-01-11 16:19:01 -0700

    Add News, PreschoolDevSkill to separate files

--- src/webpage-components/Content.js
+++ src/webpage-components/Content.js
@@ -31,7 +31,6 @@ import {
 import "./../index-src/css/Content.css"
 
 import WebFont from 'webfontloader';
-
 import Sammy from "./../images/SammyPage.jpg"
 import SammySchool from "./../images/Sammy_School_Image.jpg"
 import SammyReading from "./../images/Sammy_Reading_Image.jpg"
@@ -48,10 +47,10 @@ import SammyTestimonials from "./../images/Sammy_Testimonials_Image.jpg"
 import { FaFax } from 'react-icons/fa';
 import { FaPhone } from 'react-icons/fa';
 
-
 import Home from "./content-components/home/Home.js"
 import ProgramFees from "./content-components/registration/registration/ProgramFees.js"
 import Programs from "./content-components/programs/Programs.js"
+import PreschoolDevelopmentSkills from "./content-components/programs/PreschoolDevelopmentSkills.js"
 import Committee from "./content-components/registration/parent_involvement/Committee.js"
 import ParentInvolvement from "./content-components/registration/parent_involvement/ParentInvolvement.js"
 import GeneralGuidelines from "./content-components/registration/GeneralGuidelines.js"
@@ -60,6 +59,8 @@ import Resources from "./content-components/Resources.js"
 import OurStaff from "./content-components/about_us/OurStaff.js"
 import OurBoard from "./content-components/about_us/OurBoard.js"
 import News from "./content-components/news/News.js"
+
+
 export default class Content extends Component {
 
 	constructor(props){
@@ -253,7 +254,7 @@ export default class Content extends Component {
 									<Helmet>
 										<title>{this.props.titles["PreschoolDevSkills"]}</title>
 									</Helmet>
-									{this.PreschoolDevSkills()}
+									<PreschoolDevelopmentSkills />
 								</div>
 							</Route>
 							<Route path={this.props.site_urls["IsMyChildReady"]} exact>
@@ -568,7 +569,7 @@ export default class Content extends Component {
 				The Kindergarden curriculum overlaps the goals and guidelines laid out for the Preschool students; the expectation is the Kindergarden students have already had exposure to these goals or will have by the end of the school year.
 				Our goal for all children in our program is to be Kindergarden ready.</p>
 
-				<p><b>Please see <a className= "green" href={this.props.site_urls["Facebook"]}> Preschool Development Skills</a> for more details on the goals and expectations for students.</b></p>
+				<p><b>Please see <a className= "green" href={this.props.site_urls["PreschoolDevSkills"]}> Preschool Development Skills</a> for more details on the goals and expectations for students.</b></p>
 
 				<p><b>Children who turn 5 before March 1st in the upcoming school year (September - May)</b> are eligible to <b>attend Kindergarden in Alberta</b>.
 				Students who qualify for Kindergarden attend 5 half days /week, unless an alternate path has been approved by the teacher and parents.</p>
@@ -729,78 +730,6 @@ export default class Content extends Component {
 
 			</div>
 
-		);
-	}
-
-	PreschoolDevSkills(){
-		return(
-			<div className="info">
-				<p className="fun">Preschool Development Skills</p>
-				<p><b>The LCDS Preschool is a structured play oriented program which focuses on developing the child’s social, motor and personal development skills.</b></p>
-
-				<p className="subTitle">Social Skills are developed by:</p>
-				<ul>
-					<li>Learning responsibility for personal belongings.</li>
-					<li>Cooperating and sharing with others.</li>
-					<li>Learning to take turns.</li>
-					<li>Learning how to listen when a friend is speaking.</li>
-					<li>Practicing to be friendly and helpful.</li>
-					<li>Sitting and listening quietly.</li>
-					<li>Speaking in a clear, confident, quiet voice, using words to express their feelings.</li>
-				</ul>
-				<p className="subTitle">Motor Skills are developed by:</p>
-				<ul>
-					<li>Large muscle control is developed by engaging in activities which include body movement.</li>
-					<li>Small muscle control is developed by engaging in activities such as cutting with scissors, gluing & pasting, painting, coloring, etc.</li>
-					<li>Learning healthy habits such as hand washing, learning to cover your sneeze or cough, learning to use a tissue and dispose of it appropriately, etc.</li>
-				</ul>
-				<p className="subTitle">Personal Skills are developed by:</p>
-				<ul>
-					<li>Learning to adjust and be happy in a group setting when away from home.</li>
-					<li>Learning to make new friends and not be shy.</li>
-					<li>Learning to control their emotions (tempers, tears, anger, jealousy, frustration, etc.)</li>
-					<li>Learning to be proud of their accomplishments.</li>
-					<li>Developing language and listening skills.</li>
-					<li>Awareness of the world and people around them.</li>
-					<li>Being able to think for themselves and share ideas in a group.</li>
-					<li>Developing curiosity, creativity and imagination.</li>
-					<li>Understanding and enjoying holidays and seasons.</li>
-					<li>Learning to use different mediums to create projects (paper, glue, fabric, crayons, paint, play dough or clay, etc.)</li>
-					<li>Learn to follow steps to re create an example for a craft or project.</li>
-				</ul>
-				<p className="subTitle">Language Skills are developed by:</p>
-				<ul>
-					<li>Observing appropriate vocabulary and language use modeled by staff members.</li>
-					<li>Interaction with staff members through out class time.</li>
-					<li>Introducing Songs and Rhymes.</li>
-					<li>Repetition of sounds and words during class activities.</li>
-					<li>Introduction to Letters & Numbers (Letters of the week, calendar, etc.)</li>
-					<li>Staff members use fun strategies (games, songs, stories, group conversation) to assist in developing language and vocabulary use.</li>
-				</ul>
-
-
-
-				<p className= "subTitle">The above goals are achieved through the following classroom activities:</p>
-				<ul>
-					<li>Free play time for the students to interact and practice appropriate social skills.  </li>
-					<li>Staff model appropriate behavior which is acceptable in the classroom for the children to follow.</li>
-					<li>Staff model appropriate dialog for the children to use and are encouraged to use their words to express themselves.</li>
-					<li>A variety of materials and activities are available to the children to play with.  </li>
-					<li>Activities and materials are changed regularly to provide enhancement to the topics of study introduced to the students.</li>
-					<li>Daily songs and group conversations.</li>
-					<li>Daily movement games and actions to music or songs.</li>
-					<li>Story and Library time.</li>
-					<li>Daily projects which may include painting, coloring, writing, gluing and cutting.</li>
-					<li>Sharing Days.</li>
-					<li>Regular opportunities to speak and share in front of the class.</li>
-					<li>Class parties for celebration of holidays.</li>
-					<li>Helping Hand days.</li>
-					<li>Regularly planned field trips in and out of the classroom.</li>
-					<li>Cooking days.</li>
-					<li>Daily class routine.</li>
-					<li>Daily snack time.</li>
-				</ul>
-			</div>
 		);
 	}
 
--- src/webpage-components/content-components/news/News.js
+++ src/webpage-components/content-components/news/News.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class Programs extends Component {
+export default class News extends Component {
 
   constructor(props) {
     super(props)
